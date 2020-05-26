43708e9055aadff6b6ad169c37c9da1763e49182
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-05-26 16:56:09 +0800

    add color box, basic styling color box and palette

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors} />
+      <Palette {...seedColors[5]} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map(c => (
+      <ColorBox background={c.color} />
+    ));
     return (
       <div className="Palette">
         <div className="Palette-colors">
-          
+          {colorBoxes}
         </div>
       </div>
     );
