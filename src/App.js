0e0ee60bee1e19436775ed24aac1d257a249faca
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-05-28 02:34:11 +0800

    fix individual palette route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { 
   BrowserRouter as Router,
   Switch,
-  Link,
   Route } from 'react-router-dom';
 import seedColors from './seedColors';
 import Palette from './Palette';
 import { generatePalette } from './colorHelpers';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/' render={() => <h1>palette list goes here</h1>} />
-        <Route exact path='/palette/:id' render={() => <h1>palette</h1>} />
-      </Switch>
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
-    </Router>
-  );
+class App extends Component { 
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    })
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' render={() => <h1>palette list goes here</h1>} />
+          <Route exact 
+            path='/palette/:id' 
+            render={
+              (routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+            }
+          />
+        </Switch>
+        {/* <div className="App">
+          <Palette palette={generatePalette(seedColors[4])} />
+        </div> */}
+      </Router>
+    );
+  }
 }
 
 export default App;
