e277ce9151ea556287da09e702113be40491e4a0
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-05-28 02:06:43 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route } from 'react-router-dom';
 import seedColors from './seedColors';
 import Palette from './Palette';
 import { generatePalette } from './colorHelpers';
@@ -6,9 +11,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' render={() => <h1>palette list goes here</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>palette</h1>} />
+      </Switch>
+      <div className="App">
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    </Router>
   );
 }
 
