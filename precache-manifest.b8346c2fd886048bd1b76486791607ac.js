116b6ac22c128006589e42dce42d1d4f80e62eae
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-06-13 19:28:33 +0800

    Updates

---
+++ precache-manifest.b8346c2fd886048bd1b76486791607ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cc689972773abd28254713895920cb",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "f87f40acf98953e712b5",
+    "url": "/colors-app/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "f79b8c933028c58acf5f",
+    "url": "/colors-app/static/css/main.cefeaf2f.chunk.css"
+  },
+  {
+    "revision": "f87f40acf98953e712b5",
+    "url": "/colors-app/static/js/2.98a6698f.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/colors-app/static/js/2.98a6698f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79b8c933028c58acf5f",
+    "url": "/colors-app/static/js/main.73ae422e.chunk.js"
+  },
+  {
+    "revision": "073cf403b162e76ee8a1",
+    "url": "/colors-app/static/js/runtime-main.ea77a7ae.js"
+  },
+  {
+    "revision": "95c7e1845a0f3e17a3e21706c579aeeb",
+    "url": "/colors-app/static/media/bg.95c7e184.svg"
+  }
+]);
